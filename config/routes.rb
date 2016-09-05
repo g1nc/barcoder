a05b08275d632e840bfc1b3af628b4d5291420e5
g1nc <grimm.hellg@gmail.com>
2016-09-06 01:30:36 +0300

    Added statis pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  match 'contact', to: 'pages#contact', via: [:get]
+  match 'about',   to: 'pages#about',   via: [:get]
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
