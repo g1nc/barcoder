389ef7f461ea1237c2fc695b3bf3fc4ef78ef6d8
g1nc <grimm.hellg@gmail.com>
2016-09-07 00:43:28 +0300

    Filling Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :skip => :registration
+  devise_for :users
 
   match 'contact', to: 'pages#contact', via: [:get]
   match 'about',   to: 'pages#about',   via: [:get]
