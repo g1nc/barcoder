573e7ff4ed60dfc8b5eec5e14c8eaae44de71e61
g1nc <grimm.hellg@gmail.com>
2016-09-19 16:52:20 +0300

    Changes to db

--- db/migrate/20160916232640_create_events.rb
+++ db/migrate/20160916232640_create_events.rb
@@ -1,7 +1,6 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-      t.integer :user_id
       t.integer :topic_id
       t.string :title
       t.datetime :date
