2d5a6384444dec1669286075825aad0618dabd32
g1nc <grimm.hellg@gmail.com>
2016-09-28 02:45:52 +0300

    Translation to view files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,11 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
     def set_event
       @event = Event.find(params[:id])
