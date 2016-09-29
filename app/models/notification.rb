e351ce0472f53648f47ff6ad48ab9d120d603a51
g1nc <grimm.hellg@gmail.com>
2016-09-29 15:40:29 +0300

    Change FCM logic for Notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,21 +8,25 @@ class Notification < ApplicationRecord
   
   private
     def send_notification
-      if self.topic.contacts
-        registration_ids = [] # an array of one or more client registration tokens
-        self.topic.contacts.each do |contact|
-          registration_ids.push(contact.token)
-          self.contacts << contact
-        end
+      # if self.topic.contacts
+      #   registration_ids = [] # an array of one or more client registration tokens
+      #   self.topic.contacts.each do |contact|
+      #     registration_ids.push(contact.token)
+      #     self.contacts << contact
+      #   end
 
-        fcm = FCM.new(self.user.token, verify: false)
-        # you can set option parameters in here
-        #  - all options are pass to HTTParty method arguments
-        #  - ref: https://github.com/jnunemaker/httparty/blob/master/lib/httparty.rb#L29-L60
-        #  fcm = FCM.new("my_api_key", timeout: 3)
+      #   fcm = FCM.new(self.user.token, verify: false)
+      #   # you can set option parameters in here
+      #   #  - all options are pass to HTTParty method arguments
+      #   #  - ref: https://github.com/jnunemaker/httparty/blob/master/lib/httparty.rb#L29-L60
+      #   #  fcm = FCM.new("my_api_key", timeout: 3)
 
-        options = {notification: {title: self.title, body: self.text, sound: 'default'}, priority: 'high'}
-        self.response = fcm.send(registration_ids, options)[:body]
-      end
+      #   options = {notification: {title: self.title, body: self.text, sound: 'default'}, priority: 'high'}
+      #   self.response = fcm.send_with_notification_key("topic_#{self.topic_id}", options)[:body]
+      # end
+
+      fcm = FCM.new(self.user.token, verify: false)
+      options = {notification: {title: self.title, body: self.text, sound: 'default'}, priority: 'high'}
+      self.response = fcm.send_with_notification_key("topic_#{self.topic_id}", options)[:body]
     end
 end
